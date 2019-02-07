0c7d6f6df5460f2dd60255ed1d62a3a38ad15874
Chun Yue Leung <aryue123321@gmail.com>
2019-02-07 01:38:39 -0800

    added selected resgion in chart

--- src/oneProportion/oneProportionChart.js
+++ src/oneProportion/oneProportionChart.js
@@ -1,8 +1,16 @@
 import Chart from 'chart.js';
+import 'chartjs-plugin-annotation';
 
 export default class ChartModule{
 
   constructor(canvasEle){
+    this.color = {
+      sample: "rgba(255,0,0,0.6)",
+      binomial: "rgba(0,0,255,0.6)",
+      line: "rgba(0,255,0,0.6)",
+      box: "rgba(0,255,0,0.1)",
+      invisible: "rgba(0,255,0,0.0)",
+    };
     var ctx = canvasEle.getContext('2d');
     this.chart = new Chart(ctx, {
       type: 'bar',
@@ -13,7 +21,7 @@ export default class ChartModule{
                 data: [],
                 borderWidth: 1,
                 id: 'x-axis-1',
-                backgroundColor: "rgba(255,0,0,0.6)",
+                backgroundColor: this.color.sample,
                 hidden: false,
               },{
                 type: 'bubble',
@@ -21,7 +29,7 @@ export default class ChartModule{
                 data: [],
                 borderWidth: 0.1,
                 id: 'x-axis-2',
-                backgroundColor: "rgba(0,0,255,0.6)",
+                backgroundColor: this.color.binomial,
                 hidden: false,
               },
           ]
@@ -44,14 +52,53 @@ export default class ChartModule{
           maintainAspectRatio: true,
           tooltips: {
             mode: 'index',
+            backgroundColor: 'rgba(0,0,0,1.0)',
             callbacks: {
               title: function(tooltipItem, data){
                 let title = tooltipItem[0].xLabel || '';
                 title += ' heads';
                 return title;
+              },
+              label: function(tooltipItem, data) {
+                return  data.datasets[tooltipItem.datasetIndex].label + tooltipItem.yLabel;
               }
+            }
+          },
+          annotation:{
+            annotations:[
+              {
+                drawTime: 'afterDraw', // overrides annotation.drawTime if set
+                id: 'lowerRange', // optional
+                type: 'line',
+                mode: 'vertical',
+                scaleID: 'x-axis-0',
+                value: 0,
+                borderColor: this.color.line,
+                borderWidth: 2,
+              },
+              {
+                drawTime: 'afterDraw', // overrides annotation.drawTime if set
+                id: 'upperRange', // optional
+                type: 'line',
+                mode: 'vertical',
+                scaleID: 'x-axis-0',
+                value: 0,
+                borderColor: this.color.line,
+                borderWidth: 2,
+              },
+              {
+                drawTime: 'afterDraw', // overrides annotation.drawTime if set
+                id: 'innerRange', // optional
+                type: 'box',
+                xScaleID: 'x-axis-0',
+                yScaleID: 'y-axis-0',
+                xMin: 0,
+                xMax: 3,
+                backgroundColor: this.color.invisible,
+                borderWidth: 0,
+              }
+            ]
           }
-        }
       }
     });
   }
@@ -62,6 +109,38 @@ export default class ChartModule{
     this.chart.data.datasets[1].data = data2;
     // console.log(this.chart.data.datasets[1].data);
     this.chart.update();
+  }
+
+  updateAnnotation(lower, upper){
+    
+    const elements = this.chart.annotation.elements;
+    const labels = this.chart.data.labels;
+    if (labels.length > 0 && lower <= upper){
+      if (lower >= labels[0]){
+        elements['lowerRange'].options.borderWidth = 2;
+        elements['lowerRange'].options.value = lower;
+        elements['innerRange'].options.xMin = lower;
+      }else{
+        elements['lowerRange'].options.borderWidth = 0;
+        elements['innerRange'].options.xMin = NaN;
+      }
+      if (upper <= labels[labels.length-1]){
+        elements['upperRange'].options.borderWidth = 2;
+        elements['upperRange'].options.value = upper;
+        elements['innerRange'].options.xMax = upper;
+      }else{
+        elements['upperRange'].options.borderWidth = 0;
+        elements['innerRange'].options.xMax = NaN;
+      }
+      elements['innerRange'].options.backgroundColor = this.color.box;
+      
+    }else{
+      elements['lowerRange'].options.borderWidth = 0;
+      elements['upperRange'].options.borderWidth = 0;
+      elements['innerRange'].options.backgroundColor = this.color.invisible;
+    }
+    this.chart.update();
+
   }
 
 }
