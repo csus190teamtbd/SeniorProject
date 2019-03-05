812c60912689db02e9b15109d54761cc945e11b0
Chun Yue Leung <aryue123321@gmail.com>
2019-03-05 14:02:50 -0800

    added shift mean

--- src/oneMean/oneMean.js
+++ src/oneMean/oneMean.js
@@ -1,13 +1,19 @@
 import { dropTextFileOnTextArea, parseCsvVariableByCol } from "../util/csv.js";
 import StackedDotChart from "../util/stackeddotchart.js";
+import * as MathUtil from "/util/math.js";
 
 export class OneMean {
   constructor(OneMeanDiv) {
-    this.ele = {
-      csvTextArea: OneMeanDiv.querySelector("#csv-input"),
-      loadDataBtn: OneMeanDiv.querySelector("#load-data-btn"),
-      variableDropDown: OneMeanDiv.querySelector("#variable")
-    };
+    this.shiftMean = 0;
+    (this.shiftedData = []),
+      (this.selectedData = []),
+      (this.ele = {
+        csvTextArea: OneMeanDiv.querySelector("#csv-input"),
+        loadDataBtn: OneMeanDiv.querySelector("#load-data-btn"),
+        variableDropDown: OneMeanDiv.querySelector("#variable"),
+        shiftMeanSlider: OneMeanDiv.querySelector("#shiftMeanSlider"),
+        shiftMeanInput: OneMeanDiv.querySelector("#shiftMeanInput")
+      });
     this.datasets = [{ label: "Data", backgroundColor: "#333333", data: [] }];
     this.dataChart1 = new StackedDotChart(
       OneMeanDiv.querySelector("#data-chart-1"),
@@ -21,39 +27,63 @@ export class OneMean {
       });
 
       dropTextFileOnTextArea(this.ele.csvTextArea);
+      this.shiftMeanListener();
+
+      this.ele.variableDropDown.addEventListener("change", e => {
+        const varName = e.target.value;
+        if (varName) {
+          this.dataChart1.updateLabelName(0, varName);
+          this.updatedShiftedData(0);
+          this.selectedData = this.data[varName];
+          if (varName) this.updateData(this.selectedData);
+          document.getElementById("original-mean").innerText = this.selectedData
+            .length
+            ? MathUtil.roundToPlaces(MathUtil.mean(this.selectedData), 2)
+            : "No Data";
+        }
+      });
     };
-
-    this.ele.variableDropDown.addEventListener("change", e => {
-      const varName = e.target.value;
-      if (e.target.value) this.updateData(varName, this.data);
-    });
-
     this.loadEventListener();
+  }
+
+  shiftMeanListener() {
+    this.ele.shiftMeanInput.addEventListener("input", e => {
+      this.updatedShiftedData(Number(e.target.value) || 0);
+    });
+    this.ele.shiftMeanSlider.addEventListener("change", e => {
+      this.updatedShiftedData(Number(e.target.value) || 0);
+    });
+  }
+
+  updatedShiftedData(value) {
+    this.shiftMean = value;
+    this.ele.shiftMeanSlider.value = value;
+    this.ele.shiftMeanInput.value = value;
+    this.shiftedData = this.selectedData.map(x => x + this.shiftMean);
+    this.updateData(this.shiftedData);
   }
 
   loadVariablesToDropDown(dataSets, dropDownElement) {
     const varNames = Object.keys(dataSets);
     const html = varNames.reduce((acc, x) => {
       return (acc += `<option value="${x}">${x}</option>`);
-    }, '<option value="null">Please Select Variable</option>');
+    }, `<option value="">Please Select Variable</option>`);
 
     dropDownElement.innerHTML = html;
   }
 
-  updateData(varName, data) {
-    this.dataChart1.updateLabelName(0, varName);
-    this.dataChart1.setDataFromRaw([data[varName]]);
-    this.dataChart1.changeDotAppearance(1, undefined);
-    if (data[varName].length) {
-      let min = Math.min.apply(undefined, data[varName]);
-      let max = Math.max.apply(undefined, data[varName]);
+  updateData(data) {
+    if (data.length) {
+      this.dataChart1.setDataFromRaw([data]);
+      this.dataChart1.changeDotAppearance(1, 1);
+      let min = Math.min.apply(undefined, data);
+      let max = Math.max.apply(undefined, data);
       this.dataChart1.setScale(min, max);
     }
     this.dataChart1.chart.update();
-
-    // document.getElementById("data-mean-1").innerText = data[0].length
-    //   ? MathUtil.roundToPlaces(MathUtil.mean(data[0]), 2)
-    //   : "No Data";
+    document.getElementById("new-mean").innerText = data.length
+      ? MathUtil.roundToPlaces(MathUtil.mean(data), 2)
+      : "No Data";
     // document.getElementById("data-mean-2").innerText = data[1].length
     //   ? MathUtil.roundToPlaces(MathUtil.mean(data[1]), 2)
     //   : "No Data";
