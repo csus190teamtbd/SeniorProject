438d0503bf1f80805dda420e0157a897fa57c29f
Chun Yue Leung <aryue123321@gmail.com>
2019-04-08 03:08:44 -0700

    corrected axis name

--- src/oneProportion/chartModule.js
+++ src/oneProportion/chartModule.js
@@ -60,7 +60,7 @@ export default class ChartModule {
               },
               scaleLabel: {
                 display: true,
-                labelString: "frequency"
+                labelString: "# of samples"
               }
             }
           ],
