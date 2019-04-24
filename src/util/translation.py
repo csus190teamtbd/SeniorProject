b6695483c6b366e7f65b1cfe3772e9515e0a9276
Chun Yue Leung <aryue123321@gmail.com>
2019-04-24 03:42:50 -0700

    added python file and translated about page

---
+++ src/util/translation.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import argparse
+import csv
+import yaml
+import os
+
+def generateYMLfromCSV(csvFile, ymlFile):
+  with open(csvFile, mode='r',encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='\t')
+    english = dict()
+    spanish  = dict()
+    for en, es, key in iter(csv_reader):
+      print(en)
+      if key:
+        category, subKey = key.split('.')
+        if category not in english:
+          english[category] = dict()
+          spanish [category] = dict()
+        english[category][subKey] = en
+        spanish [category][subKey] = es
+  f = open(ymlFile, 'w',encoding='utf-8')
+  f.write(yaml.dump({ "en": english, "es": spanish }, default_flow_style=False, allow_unicode=True))
+
+def main(args):    
+  inputFile = os.path.abspath(args.input_csv_file)
+  outputFile = os.path.abspath(args.output_yml_file)
+  generateYMLfromCSV(inputFile, outputFile)
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description=
+  """convert csv file to yml file""")
+  parser.add_argument('input_csv_file', help='input csv')
+  parser.add_argument('output_yml_file', help='output yml')
+
+  args = parser.parse_args()
+  main(args)
