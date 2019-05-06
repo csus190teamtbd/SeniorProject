7d22ab10f2fd21630aa0777266c6c289385b009d
Chun Yue Leung <aryue123321@gmail.com>
2019-05-06 12:45:14 -0700

    add acipt to build exe

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 rm -r ./dist
 python ./generateYML.py
-jekyll build -d ./dist/es --config _config.yml,_config_web.yml,_config_es.yml
-jekyll build -d ./dist/en --config _config.yml,_config_web.yml,_config_en.yml
+if [ $1 == "exe" ]
+then
+  jekyll build -d ./dist/es --config _config.yml,_config_exe.yml,_config_es.yml
+  jekyll build -d ./dist/en --config _config.yml,_config_exe.yml,_config_en.yml
+else
+  jekyll build -d ./dist/es --config _config.yml,_config_web.yml,_config_es.yml
+  jekyll build -d ./dist/en --config _config.yml,_config_web.yml,_config_en.yml
+fi
 cp ./index.html ./dist
