aa2170f9c9d4d122d5763531e4d8dc34fa1211e7
Chun Yue Leung <aryue123321@gmail.com>
2019-05-08 13:11:23 -0700

    put readme in app folder

--- buildRelease.sh
+++ buildRelease.sh
@@ -3,9 +3,11 @@ rm -r ./dist
 python3 ./generateYML.py
 if [ $1 == "exe" ]
 then
+  rm -r ./dist_electron
   jekyll build -d ./dist/es --config _config.yml,_config_exe.yml,_config_es.yml
   jekyll build -d ./dist/en --config _config.yml,_config_exe.yml,_config_en.yml
-  cp ./electron_add_on/readme.txt ./dist
+  mkdir dist_electron
+  cp ./electron_add_on/readme.txt ./dist_electron
 else
   jekyll build -d ./dist/es --config _config.yml,_config_web.yml,_config_es.yml
   jekyll build -d ./dist/en --config _config.yml,_config_web.yml,_config_en.yml
